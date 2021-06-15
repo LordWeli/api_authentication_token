1584f59c1d6266167ee28815539dd568483c7534
Wesley Nogueira <wesleysenan@gmail.com>
2021-06-14 23:40:24 -0300

    Adição de verificação de token de autenticação

--- app/controllers/api/authorization_controller.rb
+++ app/controllers/api/authorization_controller.rb
@@ -1,2 +1,13 @@
 class Api::AuthorizationController < ApplicationController
+  before_action :verify_authenticity
+
+  def verify_authenticity
+    return render json: { error: 'Invalid Token!', status: 401} unless verify_token
+  end
+
+  private
+
+  def verify_token
+    request.headers['HTTP_AUTH_TOKEN'].to_s == ENV['AUTH_TOKEN'].to_s
+  end
 end
